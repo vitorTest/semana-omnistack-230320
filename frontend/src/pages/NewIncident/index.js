31b28d76f2884043cfe70d42d87721f3d839e466
vitorTest <vytorfelix2@gmail.com>
2020-04-17 03:29:57 -0300

    day 3 - React Finished

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,43 +1,81 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 
+import api from '../../services/api'
 import './styles.css'
 
 import logoImg from '../../assets/logo.svg'
 
 export default function Register() {
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [value, setValue] = useState('')
+
+    const history = useHistory()
+
+    const ongID = localStorage.getItem('ongID')
+
+    async function handleNewIncident(e) {
+        e.preventDefault()
+
+        const data = {
+            title,
+            description,
+            value
+        }
+
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongID
+                }
+            })
+
+            history.push('/profile')
+        } catch (err) {
+            alert('Erro no cadastro, tente novamente.')
+        }
+    }
+
     return (
-        <div className="register-container">
+        <div className="new-incident-container">
             <div className="content">
                 <section>
                     <img src={logoImg} alt="Be The Hero"/>
 
-                    <h1>Cadastro</h1>
+                    <h1>Cadastrar novo caso</h1>
                     <p>
-                        Faça seu cadastro , entre na plataforma e ajude
-                        pessoas a encontrarem os casos da sua ONG.
+                        Descreva o caso detalhadamente para encontrar um 
+                        herói para resolver isso.
                     </p>
 
-                    <Link className="back-link" to="/">
+                    <Link className="back-link" to="/profile">
                         <FiArrowLeft size={16} color="#E02041" />  
-                        voltar para página inicial
+                        voltar para home
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Nome da ONG" type="text" />
-                    <input type="email" placeholder="Email" />
-                    <input placeholder="WhatsApp" />
-
-                    <div className="input-group">
-                        <input placeholder="Cidade" />
-                        <input placeholder="UF" style={{ width:80 }} />
-                    </div>
+                <form onSubmit={handleNewIncident}>
+                    <input 
+                        placeholder="Título do caso" 
+                        value={title}
+                        onChange={e => setTitle(e.target.value)} 
+                    />
+                    <textarea 
+                        placeholder="Descrição do caso" 
+                        value={description}
+                        onChange={e => setDescription(e.target.value)} 
+                    />
+                    <input 
+                        placeholder="Valor em reais"
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
 
                     <button className="button" type="submit">Cadastrar</button>
                 </form>
             </div>
         </div>
-    );
+    )
 }
